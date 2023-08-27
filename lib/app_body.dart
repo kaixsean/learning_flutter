6bb35ce34283270a0aebf4a93c0e9e8ebd0939cb
kaixsean <kxakai@gmail.com>
2023-08-27 17:46:13 +0800

    feat: add TextField

--- lib/app_body.dart
+++ lib/app_body.dart
@@ -1,51 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 class AppBody extends StatelessWidget {
   const AppBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final btn = PopupMenuButton(
-      itemBuilder: (context) {
-        return const <PopupMenuEntry>[
-          PopupMenuItem(
-            value: 1,
-            child: Text(
-              "第一項",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          PopupMenuDivider(),
-          PopupMenuItem(
-            value: 2,
-            child: Text(
-              "第二項",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          PopupMenuDivider(),
-          PopupMenuItem(
-            value: 3,
-            child: Text(
-              "第三項",
-              style: TextStyle(fontSize: 20),
-            ),
-          )
-        ];
-      },
-      color: Colors.yellow,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      offset: const Offset(100, 30),
-      onSelected: (value) => _showSnackBar(context, value.toString()),
-      onCanceled: () => _showSnackBar(context, "取消選擇"),
+    final nameController = TextEditingController();
+    final nameField = TextField(
+      controller: nameController,
+      style: const TextStyle(fontSize: 20),
+      decoration: const InputDecoration(
+          labelText: '輸入姓名', labelStyle: TextStyle(fontSize: 20)),
     );
+
+    final btn = ElevatedButton(
+        onPressed: () => _showSnackBar(context, nameController.text),
+        child: const Text('確定'));
 
     final widget = Center(
       heightFactor: 2,
-      child: btn,
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: nameField,
+            width: 200,
+            margin: const EdgeInsets.symmetric(vertical: 10),
+          ),
+          Container(child: btn, margin: const EdgeInsets.symmetric(vertical: 10),)
+        ],
+      ),
     );
-
     return widget;
   }
 
@@ -54,19 +38,8 @@ class AppBody extends StatelessWidget {
       content: Text(msg),
       duration: const Duration(seconds: 3),
       backgroundColor: Colors.blue,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      action: SnackBarAction(
-        label: 'Toast 訊息',
-        textColor: Colors.white,
-        onPressed: () => Fluttertoast.showToast(
-            msg: '你按下 SnackBar',
-            toastLength: Toast.LENGTH_LONG,
-            gravity: ToastGravity.CENTER,
-            backgroundColor: Colors.blue,
-            textColor: Colors.white,
-            fontSize: 20.0),
-      ),
     );
+
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
