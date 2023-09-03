cdb2df562347562c48010dabdbceb94c75fbdbcc
kaixsean <kxakai@gmail.com>
2023-09-03 23:14:15 +0800

    feat: 選擇興趣app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'app_body.dart';
 
 void main() => runApp(const App());
 
@@ -8,18 +7,102 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var appTitle = const Text('Flutter App');
-    var appBody = AppBody();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
 
-    var appBar = AppBar(title: appTitle);
+class MyHomePage extends StatelessWidget {
+  static const _hobbies = <String>['游泳', '唱歌', '聽音樂', '騎單車', '旅遊', '美食'];
+  final ValueNotifier<List<bool>> _hobbiesSelected =
+      ValueNotifier(List<bool>.generate(_hobbies.length, (int index) => false));
 
-    var app = MaterialApp(
-      home: Scaffold(
-        appBar: appBar,
-        body: appBody,
+  final ValueNotifier<String> _text = ValueNotifier('');
+
+  @override
+  Widget build(BuildContext context) {
+    final appBar = AppBar(title: const Text('選擇興趣'));
+
+    final btn = ElevatedButton(
+        onPressed: () => _showHobbies(), child: const Text('確定'));
+
+    final widget = Center(
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Container(
+                width: 200,
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                child: ValueListenableBuilder<List<bool>>(
+                  builder: _hobbySelectionBuilder,
+                  valueListenable: _hobbiesSelected,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: btn,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: ValueListenableBuilder<String>(
+                builder: _textWidgetBuilder,
+                valueListenable: _text,
+              ),
+            )
+          ],
+        ),
       ),
     );
 
-    return app;
+    final appHomePage = Scaffold(appBar: appBar, body: widget);
+
+    return appHomePage;
+  }
+
+  Widget _hobbySelectionBuilder(
+      BuildContext context, List<bool> hobbiesSelected, Widget? child) {
+    List<CheckboxListTile> checkboxes = [];
+
+    for (var i = 0; i < _hobbies.length; i++) {
+      checkboxes.add(CheckboxListTile(
+          title: Text(
+            _hobbies[i],
+            style: const TextStyle(fontSize: 20),
+          ),
+          value: _hobbiesSelected.value[i],
+          onChanged: (newValue) {
+            _hobbiesSelected.value[i] = newValue as bool;
+            _hobbiesSelected.value = List.from(_hobbiesSelected.value);
+          }));
+    }
+
+    final wid = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: checkboxes,
+    );
+
+    return wid;
+  }
+
+  Widget _textWidgetBuilder(BuildContext context, String text, Widget? child) {
+    final wid = Text(
+      text,
+      style: const TextStyle(fontSize: 20),
+    );
+    return wid;
+  }
+
+  _showHobbies() {
+    String selectedHobbies = '';
+    for (int i = 0; i < _hobbiesSelected.value.length; i++) {
+      if (_hobbiesSelected.value[i]) selectedHobbies += _hobbies[i];
+    }
+
+    _text.value = selectedHobbies;
   }
 }
