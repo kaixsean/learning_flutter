78d1e79f16b890e73d96b528f9c92bd66c5e21b4
kaixsean <kxakai@gmail.com>
2023-09-03 14:31:51 +0800

    feat: number picker

--- lib/app_body.dart
+++ lib/app_body.dart
@@ -1,78 +1,26 @@
 import 'package:flutter/material.dart';
-
-var citites = ['倫敦', '東京', '舊金山'];
+import 'package:numberpicker/numberpicker.dart';
 
 class AppBody extends StatelessWidget {
-  final ValueNotifier<String> _cityName = ValueNotifier('');
-  final ValueNotifier<int> _selectedCity = ValueNotifier(0);
+  final ValueNotifier<int> _age = ValueNotifier(20);
+  static const int _maxAge = 100, _minAge = 0;
 
   AppBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final btn = ElevatedButton(
-        onPressed: () {
-          _cityName.value =
-              _selectedCity.value < 0 ? '' : citites[_selectedCity.value];
-        },
-        child: const Text('確定'));
-
-    final widget = Center(
-      child: Column(
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 10),
-            child: ValueListenableBuilder<int>(
-              builder: _radioButtonBuilder,
-              valueListenable: _selectedCity,
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 10),
-            child: btn,
-          ),
-          Container(
-            child: ValueListenableBuilder<String>(
-              builder: _cityNameWidgetBuilder,
-              valueListenable: _cityName,
-            ),
-          )
-        ],
-      ),
-    );
+    final widget = ValueListenableBuilder<int>(
+        valueListenable: _age, builder: _agePickerBuilder);
     return widget;
   }
 
-  Widget _cityNameWidgetBuilder(
-      BuildContext context, String cityName, Widget? child) {
-    final widget = Text(
-      cityName,
-      style: const TextStyle(fontSize: 20),
-    );
-
-    return widget;
-  }
-
-  Widget _radioButtonBuilder(
-      BuildContext context, int selectedItem, Widget? child) {
-    var radioItems = <RadioListTile>[];
-
-    for (var i = 0; i < citites.length; i++) {
-      radioItems.add(RadioListTile(
-          value: i,
-          groupValue: _selectedCity.value,
-          title: Text(
-            citites[i],
-            style: const TextStyle(fontSize: 20),
-          ),
-          onChanged: (value) => _selectedCity.value = value));
-    }
-
-    final wid = Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: radioItems,
-    );
-
+  Widget _agePickerBuilder(
+      BuildContext context, int selectedAge, Widget? child) {
+    final wid = NumberPicker(
+        minValue: _minAge,
+        maxValue: _maxAge,
+        value: selectedAge,
+        onChanged: (newValue) => _age.value = newValue);
     return wid;
   }
 }
