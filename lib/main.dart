98201333563327b74506e7ccb65653485ebea75e
kaixsean <kxakai@gmail.com>
2023-09-12 20:21:37 +0800

    feat:  named routes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,11 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MyHomePage(),
+        '/second page': (context) => SecondPage()
+      },
     );
   }
 }
@@ -22,31 +26,7 @@ class MyHomePage extends StatelessWidget {
     final appBar = AppBar(title: const Text('切換頁面'));
 
     final btn = ElevatedButton(
-        onPressed: () => Navigator.push(
-            context,
-            PageRouteBuilder(
-                pageBuilder: (BuildContext context, Animation<double> animation,
-                        Animation<double> secondaryAnimation) =>
-                    SecondPage(),
-                transitionsBuilder: (BuildContext context,
-                        Animation<double> animation,
-                        Animation<double> secondaryAnimation,
-                        Widget child) =>
-                    ScaleTransition(
-                      scale: Tween<double>(begin: 0.0, end: 1.0).animate(
-                          CurvedAnimation(
-                              parent: animation, curve: Curves.fastOutSlowIn)),
-                      child: RotationTransition(
-                        turns: Tween<double>(
-                          begin: 0.0,
-                          end: 1.0,
-                        ).animate(
-                          CurvedAnimation(
-                              parent: animation, curve: Curves.linear),
-                        ),
-                        child: child,
-                      ),
-                    ))),
+        onPressed: () => Navigator.pushNamed(context, '/second page'),
         child: const Text('開啟第二頁'));
 
     final widget = Container(
